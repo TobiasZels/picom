86a84bf6252e360fd110c61f637bdaaf8c37cc16
Tobias Zels <T.smi@gmx.de>
2023-08-23 16:23:34 +0200

    updated Marker

--- study-scripts/frame_creator/create_frame.py
+++ study-scripts/frame_creator/create_frame.py
@@ -4,117 +4,117 @@ import time
 import numpy as np
 import os
 
-def change_brightness(img, value=30):
-    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    h, s, v = cv2.split(hsv)
-    v = cv2.add(v,value)
-    v[v > 255] = 255
-    v[v < 0] = 0
-    final_hsv = cv2.merge((h, s, v))
-    img = cv2.cvtColor(final_hsv, cv2.COLOR_HSV2BGR)
-    return img
-
-i = 0
-qrCrode = cv2.imread("marker.png")
-scale_percent = 1000
-
-width = int(qrCrode.shape[1] * scale_percent / 100)
-height = int(qrCrode.shape[0] * scale_percent / 100)
-
-dsize = (width, height)
-
-qrCrode = cv2.resize(qrCrode, dsize)
-
-h = qrCrode.shape[0]
-w = qrCrode.shape[1]
-
-og = cv2.imread("frame.png")
-og = cv2.resize(og,(1920, 1080),interpolation=cv2.INTER_AREA)
-x1 = cv2.imread("frame.png")
-x1 = cv2.resize(x1,(1920, 1080),interpolation=cv2.INTER_AREA)
-
-x2 = cv2.imread("frame.png")
-x2 = cv2.resize(x2,(1920, 1080),interpolation=cv2.INTER_AREA)
-
-l = 30
-r_l = l #* 0.2126
-g_l = l #* 0.7152
-b_l = l #* 0.0722
-
-xOffset = 0
-yOffset = 0
-
-for y in range(0, og.shape[0]):
-    print(y/og.shape[0])
-    for x in range(0, og.shape[1]):
-        rl = r_l
-        gl = g_l
-        bl = b_l
-
-        r, g,b = og[y][x]
-
-        r = (r * (235) / 255) + 10
-        g = (g * (235) / 255) + 10
-        b = (b * (235) / 255) + 10
-
-        if(r + rl > 255): 
-            rl = 255-r
-        if(g + gl > 255): 
-            gl = 255-g
-        if(b + bl > 255): 
-            bl = 255-b
+def create_marker(frame):
+    create_Frames("marker1.png", frame, 400)
+    create_Frames("marker2.png", frame, 1000)
+    create_Frames("marker3.png", frame, 75)
 
 
-        if(r - rl < 0):
-            rl = 0+r
-        if(g - gl < 0):
-            gl = 0+g
-        if(b - bl < 0):
-            bl = 0+b
+def create_Frames(marker, frame, scaling):
+    i = 0
+    qrCrode = cv2.imread(marker)
 
-        x1[y][x] = (r+rl, g+gl, b+bl)
-        x2[y][x] = (r-rl, g-gl, b-bl)
+    width = int(qrCrode.shape[1] * scaling / 100)
+    height = int(qrCrode.shape[0] * scaling / 100)
+
+    dsize = (width, height)
+
+    qrCrode = cv2.resize(qrCrode, dsize)
+
+    h = qrCrode.shape[0]
+    w = qrCrode.shape[1]
+
+    og = cv2.imread(frame)
+    og = cv2.resize(og,(1920, 1080),interpolation=cv2.INTER_AREA)
+    x1 = cv2.imread(frame)
+    x1 = cv2.resize(x1,(1920, 1080),interpolation=cv2.INTER_AREA)
+
+    x2 = cv2.imread(frame)
+    x2 = cv2.resize(x2,(1920, 1080),interpolation=cv2.INTER_AREA)
+
+    l = 30
+    r_l = l #* 0.2126
+    g_l = l #* 0.7152
+    b_l = l #* 0.0722
+
+    xOffset = int((1920-width) / 2)
+    yOffset = int((1080-height) /2)
+
+    for y in range(0, og.shape[0]):
+        print(y/og.shape[0])
+        for x in range(0, og.shape[1]):
+            rl = r_l
+            gl = g_l
+            bl = b_l
+
+            r, g,b = og[y][x]
+
+            r = (r * (235) / 255) + 10
+            g = (g * (235) / 255) + 10
+            b = (b * (235) / 255) + 10
+
+            if(r + rl > 255): 
+                rl = 255-r
+            if(g + gl > 255): 
+                gl = 255-g
+            if(b + bl > 255): 
+                bl = 255-b
 
 
-for y in range(0,h):
-    print(y/h)
-    for x in range(0,w):
+            if(r - rl < 0):
+                rl = 0+r
+            if(g - gl < 0):
+                gl = 0+g
+            if(b - bl < 0):
+                bl = 0+b
 
-        rl = r_l
-        gl = g_l
-        bl = b_l
-
-        r, g,b = og[y + yOffset][x + xOffset]
-
-        r = (r * (235) / 255) + 10
-        g = (g * (235) / 255) + 10
-        b = (b * (235) / 255) + 10
-
-        if(r + rl > 255): 
-            rl = 255-r
-        if(g + gl > 255): 
-            gl = 255-g
-        if(b + bl > 255): 
-            bl = 255-b
+            x1[y][x] = (r+rl, g+gl, b+bl)
+            x2[y][x] = (r-rl, g-gl, b-bl)
 
 
-        if(r - rl < 0):
-            rl = 0+r
-        if(g - gl < 0):
-            gl = 0+g
-        if(b - bl < 0):
-            bl = 0+b
+    for y in range(0,h):
+        print(y/h)
+        for x in range(0,w):
 
-        if np.sum(qrCrode[y][x]) < 200:
-            x1[y + yOffset][x + xOffset] = (r-rl, g-gl, b-bl)
-            x2[y + yOffset][x + xOffset] = (r+rl, g+gl, b+bl)
-        else:
-            x1[y + yOffset][x + xOffset] = (r+rl, g+gl, b+bl)
-            x2[y + yOffset][x + xOffset] = (r-rl, g-gl, b-bl)
+            rl = r_l
+            gl = g_l
+            bl = b_l
 
-cv2.imwrite("output1.png", x1)
-cv2.imwrite("output2.png", x2)
+            r, g,b = og[y + yOffset][x + xOffset]
 
+            r = (r * (235) / 255) + 10
+            g = (g * (235) / 255) + 10
+            b = (b * (235) / 255) + 10
+
+            if(r + rl > 255): 
+                rl = 255-r
+            if(g + gl > 255): 
+                gl = 255-g
+            if(b + bl > 255): 
+                bl = 255-b
+
+
+            if(r - rl < 0):
+                rl = 0+r
+            if(g - gl < 0):
+                gl = 0+g
+            if(b - bl < 0):
+                bl = 0+b
+
+            if np.sum(qrCrode[y][x]) < 200:
+                x1[y + yOffset][x + xOffset] = (r-rl, g-gl, b-bl)
+                x2[y + yOffset][x + xOffset] = (r+rl, g+gl, b+bl)
+            else:
+                x1[y + yOffset][x + xOffset] = (r+rl, g+gl, b+bl)
+                x2[y + yOffset][x + xOffset] = (r-rl, g-gl, b-bl)
+
+    cv2.imwrite("output1 .png" + frame + marker, x1)
+    cv2.imwrite("output2.png" + frame + marker, x2)
+
+
+create_marker("frame1.png")
+create_marker("frame2.png")
+create_marker("frame3.png")
 #Y = 0.2126 R + 0.7152 G + 0.0722 B
 
 #video = cv2.VideoWriter('video.avi', 0, 120, (width,height))
